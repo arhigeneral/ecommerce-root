c855157035eebd63d13f7f9b215a10778ac9e748
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-24 19:41:15 +0300

    Templates added!

--- shop/views.py
+++ shop/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(ruqest):
-    return HttpResponse("<h1>Home</h1>")
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
