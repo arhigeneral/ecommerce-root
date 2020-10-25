38818ccb607422dad014cb2530282add2a660f78
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-25 23:04:24 +0300

    Images added

--- shop/views.py
+++ shop/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
-def about(request):
-    return render(request, 'about.html')
+def slut(request):
+    return render(request, 'slut.html')
