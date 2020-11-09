253024263889cbf350c50169c2ad79a3ed38675d
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-11-09 16:41:17 +0300

    context processors added

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('<slug>:category_slug>', views.home, name = 'products_by_category'),
+    path('<slug:category_slug>', views.home, name = 'products_by_category'),
     path('slut/', views.slut, name = 'slut'),
 ]
