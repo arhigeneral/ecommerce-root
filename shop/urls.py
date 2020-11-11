d1c9aa5a9cb916c5dd23f2226bf5434cd803a5fb
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-11-11 03:41:44 +0300

    Cart page added but i dont know how to go there:

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('<slug:category_slug>', views.home, name = 'products_by_category'),
-    path('slut/', views.slut, name = 'slut'),
+    path('<slug:category_slug>/<slug:product_slug>', views.slut, name = 'product_detail'),
+    path('cart', views.cart_detail, name = 'cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name = 'add_cart'),
 ]
